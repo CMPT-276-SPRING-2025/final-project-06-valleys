24f5d462cbae550a1a67875a72a8b50d6f64aee6
Skimchhorn <kcs11@sfu.ca>
2025-03-16 22:16:17 -0700

    feat: create folders for setup route for handling  generateEmail and quiz features

---
+++ src/src/components/navBar.jsx
@@ -0,0 +1,26 @@
+import * as React from "react"
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+
+
+
+<NavigationMenu>
+<NavigationMenuList>
+  <NavigationMenuItem>
+    <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+    <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+    <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+    <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+    <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+    <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+  </NavigationMenuItem>
+</NavigationMenuList>
+</NavigationMenu>
