b9dad38aaf103f4801089545145479cf1753c722
Skimchhorn <kcs11@sfu.ca>
2025-03-16 23:43:05 -0700

    feat: add navBar to the layout so all pages have this navbar

---
+++ src/src/components/shieldlogo.jsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+
+export default function ShieldLogo() {
+  return (
+    <svg
+      width="40"
+      height="40"
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M24 4L8 12V24C8 34.4 14.8 42.4 24 44C33.2 42.4 40 34.4 40 24V12L24 4Z"
+        stroke="#1E40AF" 
+        strokeWidth="5"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
