43487e50a2f17d3b0b09ce439508eb418a2d4495
Philip <philip03ho@gmail.com>
2025-03-25 17:46:19 -0700

    fix: adjust workflow files and next config

--- src/next.config.mjs
+++ src/next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    outputFileTracingRoot: process.env.NODE_ENV === 'production' 
-      ? './' 
-      : undefined
-  }
 };
 
 export default nextConfig;
