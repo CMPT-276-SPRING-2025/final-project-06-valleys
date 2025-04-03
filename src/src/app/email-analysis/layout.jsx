19346ef5f68073a7b2603302bb4c109817de2bda
AmrajKoonar <amrajk31@gmail.com>
2025-04-02 21:33:13 -0700

    feat: Change page name code added into layout.jsx for email analysis.

--- src/src/app/email-analysis/layout.jsx
+++ src/src/app/email-analysis/layout.jsx
@@ -1,1 +1,22 @@
 //created layout.jsx for changing page name
+export const metadata = {
+  title: "Email Analysis | Deep Phishing",
+  description:
+    "AI-powered tool to analyze emails for phishing indicators and threats.",
+  keywords: [
+    "email phishing analysis",
+    "phishing detection",
+    "analyze email content",
+    "cybersecurity email scanner",
+    "phishing email detector",
+    "deep phishing",
+    "email header inspection",
+    "malicious email analysis",
+    "email threat scanner",
+    "AI phishing analysis",
+  ],
+};
+
+export default function EmailAnalysisLayout({ children }) {
+  return <>{children}</>;
+}
