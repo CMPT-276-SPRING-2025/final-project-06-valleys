8c2e5f13581c6c33ebe17ce96a6ce837a4474ede
AmrajKoonar <amrajk31@gmail.com>
2025-04-02 21:29:18 -0700

    feat: Change page name code added into layout.jsx

--- src/src/app/quiz/layout.jsx
+++ src/src/app/quiz/layout.jsx
@@ -1,1 +1,11 @@
-//create layout.jsx for changing page name
+//created layout.jsx for changing page name
+
+export const metadata = {
+  title: "Email Quiz | Deep Phishing",
+  description:
+    "Test your ability to identify phishing emails with realistic examples.",
+};
+
+export default function QuizLayout({ children }) {
+  return <>{children}</>;
+}
